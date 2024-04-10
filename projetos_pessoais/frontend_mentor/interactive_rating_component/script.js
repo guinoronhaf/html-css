de76741519f35a32d4ccdeb3d2fbdf4545a90577
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-04-09 22:06:10 -0300

    refazendo o projeto

--- projetos_pessoais/frontend_mentor/interactive_rating_component/script.js
+++ projetos_pessoais/frontend_mentor/interactive_rating_component/script.js
@@ -1,7 +1,22 @@
-let box = document.querySelector('.box')
+const rating = document.querySelectorAll('.num')
+const rated = document.getElementById('message')
+const submitButton = document.getElementById('submit')
+const form = document.getElementById('form')
+const feedback = document.getElementById('feedback')
 
-
-function clicou() {
-    box.style.backgroundColor = 'hsl(217, 12%, 63%)'
-    box.style.color = 'white'
+for (let i = 0; i < rating.length; i++) {//rating.lenght é justamente a quantidade de elementos com a class "rating". Ademais, esse "for" é necessário para selecionar todos os elementos dessa classe, e não só o primeiro, como indica o padrão.
+    rating[i].addEventListener("click", function(e) {
+        e.preventDefault() //retira as ações padrão
+        rated.innerText = rating[i].innerText
+    })
 }
+
+submitButton.addEventListener("click", function (e) {
+    e.preventDefault()
+    if (rated.innerText !== '') {
+        form.style.display = 'none'
+        feedback.style.display = 'flex'
+    } else {
+        alert('Por favor, selecione uma das opções de nota.')
+    }
+})
