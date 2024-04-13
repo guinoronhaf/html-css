ad9ab2b4b24634ff1795368ca9019bb70b2a8143
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-04-12 21:28:49 -0300

    ajustes

--- projetos_pessoais/frontend_mentor/age_calculator_app/script.js
+++ projetos_pessoais/frontend_mentor/age_calculator_app/script.js
@@ -1,13 +1,8 @@
-let day = document.getElementById('iday')
-let d = Number(day.value)
-let month = document.getElementById('imonth')
-let m = Number(month.value)
-let year = document.getElementById('iyear')
-let y = Number(year.value)
-let button = document.querySelector('.circle')
 
-button.addEventListener('click', submit())
-
-function submit() {
-    
-}
+//var agora = new Date()
+//var dia = agora.getDay() 0 a 6 com 0 sendo o domingo
+//var mes = agora.getMonth() 0  a 11 com 0 sendo janeiro
+//var ano = agora.getFullYear() ano padrao
+/*console.log(dia)
+console.log(mes)
+console.log(ano)*/
