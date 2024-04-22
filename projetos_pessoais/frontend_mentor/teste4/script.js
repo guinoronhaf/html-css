528d1606eeaad2b69946dadbda8fb73857d489cf
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-04-22 13:26:49 -0300

    tip calculator app

--- projetos_pessoais/frontend_mentor/teste4/script.js
+++ projetos_pessoais/frontend_mentor/teste4/script.js
@@ -1,7 +1,16 @@
 const fUser = document.querySelector('#f_user')
+const fPeople = document.querySelector('#f_people')
 const btnTips = [...document.querySelectorAll('#tips > input, button')]
 const number = document.querySelector('.number')
 const custom = document.querySelector('#custom')
+
+fPeople.addEventListener("input", () => {
+    if (!fPeople.value || fPeople.value <= 0) {
+        fPeople.classList.add('wrong')
+    } else {
+        fPeople.classList.remove('wrong')
+    }
+})
 
 btnTips.map((el) => {
     if (el != btnTips[5]) {
@@ -9,21 +18,21 @@ btnTips.map((el) => {
             e.preventDefault()
             custom.value = 'Custom'
             let t = el.value
-            if (fUser.value) {
-                let finalValue = fUser.value * (t/100)
+            if (fUser.value && fPeople.value > 0) {
+                let finalValue = (fUser.value * (t/100))/fPeople.value
                 number.textContent = finalValue
             } else {
-                alert('Por favor, selecione uma das opções ou digite um valor customizado.')
+                alert('Por favor, selecione uma das opções ou digite um valor customizado e defina o número válido de pessoas.')
             }
         })
     } else {
         el.addEventListener("input", () => {
             let t = el.value
-            if (fUser.value) {
-                let finalValue = fUser.value * (t/100)
+            if (fUser.value && fPeople.value > 0) {
+                let finalValue = (fUser.value * (t/100))/fPeople.value
                 number.textContent = finalValue
             } else {
-                alert('Por favor, selecione uma das opções ou digite um valor customizado.')
+                alert('Por favor, selecione uma das opções ou digite um valor customizado e defina o número válido de pessoas.')
                 custom.value = 'Custom'
             }
         })
--- projetos_pessoais/frontend_mentor/tip_calculator_app/script.js
+++ projetos_pessoais/frontend_mentor/tip_calculator_app/script.js
@@ -1,58 +1,82 @@
-const bill = document.querySelector('#ibill')
-const divBill = document.querySelector('.bill_values')
-let tip = document.getElementsByTagName('button')
-const custom = document.querySelector('#custom')
-const divPeople = document.querySelector('.people_values')
-const people = document.querySelector('#ipeople')
+const fBill = document.querySelector('#f_bill')
+const infosInput = [...document.querySelectorAll('.infos-input')]
+const infosTipBtn = [...document.querySelector('#infos-tip-btn').children]
+const error = [...document.querySelectorAll('.error')]
+const fPeople = document.querySelector('#f_people')
 const val1 = document.querySelector('#val1')
 const val2 = document.querySelector('#val2')
-const resetButton = document.querySelector('#reset')
+const reset = document.querySelector('#reset')
+const custom = document.querySelector('#f_tip')
+custom.setAttribute('placeholder', 'Custom')
 
-function billv(){
-    if (bill.value == '' || bill.value < 1) {
-        divBill.classList.add('wrong')
-        return false
+fBill.addEventListener("input",() => {
+    if (fBill.value <= 0) {
+        fBill.parentNode.classList.add('wrong')
+        error[0].classList.remove('disappear')
     } else {
-        divBill.classList.remove('wrong')
-        return true
+        fBill.parentNode.classList.remove('wrong')
+        error[0].classList.add('disappear')
     }
-}
+}) 
 
-function customv() {
-    if (custom.value != '') {
-        return true
+fPeople.addEventListener("input",() => {
+    if (fPeople.value <= 0) {
+        fPeople.parentNode.classList.add('wrong')
+        error[1].classList.remove('disappear')
     } else {
-        return false
+        fPeople.parentNode.classList.remove('wrong')
+        error[1].classList.add('disappear')
     }
-}
+})
 
-function peoplev() {
-    if (people.value < 1) {
-        divPeople.classList.add('wrong')
-        return false
-    } else {
-        divPeople.classList.remove('wrong')
-        return true
-    }
-}
-
-for (let i = 0; i < tip.length; i++) {
-    tip[i].addEventListener("click", function(e) {
-        e.preventDefault()
-        let v = tip[i].value
-        if (billv() && peoplev()) {
-            val1.innerHTML = (bill.value*(v/100))/people.value
-            val2.innerHTML = bill.value/people.value
-        } else {
-            alert('Valores faltando.')
-        }
+infosInput.map((el) => {
+    el.addEventListener("mouseenter", () => {
+        el.classList.add('typing')
     })
-}
+    el.addEventListener("mouseleave", () => {
+        el.classList.remove('typing')
+    })
+})
 
+infosTipBtn.map((el) => {
+    if (el != infosTipBtn[5]) {
+        el.addEventListener("click", (e) => {
+            e.preventDefault()
+            custom.value = 'Custom'
+            let t = el.value
+            if (fBill.value > 0 && fPeople > 0) {
+                let finalValue1 = (fBill.value * t/100)/fPeople.value
+                val1.innerHTML = `$${finalValue1.toFixed(2)}`
+                let finalValue2 = (fBill.value/fPeople.value) + finalValue1
+                val2.innerHTML = `$${finalValue2.toFixed(2)}`
+            } else {
+                alert('Make sure you fill "Bill" and "Number of People" with valid values before selecting the tip.')
+            }
+        })
+    } else {
+        el.addEventListener("input", () => {
+            let t = el.value 
+            if (fBill.value > 0 && fPeople.value > 0) {
+                let finalValue1 = (fBill.value * t/100)/fPeople.value
+                val1.innerHTML = `$${finalValue1.toFixed(2)}`
+                let finalValue2 = (fBill.value/fPeople.value) + finalValue1
+                val2.innerHTML = `$${finalValue2.toFixed(2)}`
+            } else {
+                alert('Make sure you fill "Bill" and "Number of People" with valid values before selecting the tip.')
+                custom.value = 'Custom'
+            }
+        })
+    }
+})
 
-resetButton.addEventListener("click", function(e) {
-    val1.innerHTML = '0.00'
-    val2.innerHTML = '0.00'
-    divPeople.classList.remove('wrong')
-    divBill.classList.remove('wrong')
+reset.addEventListener("click", () => {
+    error.forEach((el) => {
+        el.classList.add('disappear')
+    })
+    infosInput.forEach((el) => {
+        el.classList.remove('wrong')
+    })
+    val1.textContent = '$0.00'
+    val2.textContent = '$0.00'
+    custom.value = 'Custom'
 })
