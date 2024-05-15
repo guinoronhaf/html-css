8c25f74094638a07b530f452b3f3976220f258b8
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-05-14 21:28:03 -0300

    newsletter sign up

--- projetos_pessoais/frontend_mentor/newsletter_sign_up/script.js
+++ projetos_pessoais/frontend_mentor/newsletter_sign_up/script.js
@@ -10,26 +10,23 @@ let eFlag = false
 f_email.addEventListener("input", () => {
     const regex = /([a-z]{1,})([0-9]{1,})?@([a-z]{1,})?\.(com)\.?([a-z])?/
     if (regex.test(f_email.value)) {
+        p_error.classList.add('hide')
+        f_email.classList.remove('error')
+        f_email.style.border = '1px solid hsl(231, 7%, 60%)'
         eFlag = true
     } else {
+        p_error.classList.remove('hide')
+        f_email.classList.add('error')
+        f_email.style.border = '1px solid hsl(4, 100%, 67%)'
         eFlag = false
     }
 })
 
 submit.addEventListener("click", () => {
     if (eFlag) {
-        console.log('ok')
-        p_error.classList.add('hide')
-        f_email.classList.remove('error')
-        f_email.style.border = '1px solid hsl(231, 7%, 60%)'
         login_container.classList.add('hide')
         success_container.classList.remove('hide')
-        success_email.textContent = f_email.value
-    } else {
-        p_error.classList.remove('hide')
-        f_email.classList.add('error')
-        f_email.style.border = '1px solid hsl(4, 100%, 67%)'
-        // f_email.style.bakgroundColor = 'rgba(255, 0, 0, 0.192)'
+        success_email.innerHTML = f_email.value
     }
 })
 
