9e2cc4f6a9e6ee0db1cf3edc7521e7d9432303a4
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-05-16 22:02:56 -0300

    advice generator app

---
+++ projetos_pessoais/frontend_mentor/advice_generator_app/script.js
@@ -0,0 +1,27 @@
+// DOM elements
+const adviceTitle = document.querySelector('#advice_title');
+const adviceContent = document.querySelector('#advice_content');
+const dice = document.querySelector('#dice');
+
+// async function to get data by API
+async function getAdvice() {
+    
+    const endpoint = 'https://api.adviceslip.com/advice';
+
+    const res = await fetch(endpoint)
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+            const id = data.slip.id;
+            const advice = data.slip.advice;
+            adviceTitle.innerText = `ADVICE #${id}`;
+            adviceContent.innerText = `"${advice}"`;
+        })
+        .catch(err => console.log(`ERROR: ${err}`))
+
+};
+
+// setting events
+dice.addEventListener("click", () => {
+    getAdvice()
+});
