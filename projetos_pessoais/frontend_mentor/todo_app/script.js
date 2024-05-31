58824e1048e1774e6486687fd72411634a128b95
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-05-30 23:35:26 -0300

    ok

--- projetos_pessoais/frontend_mentor/todo_app/script.js
+++ projetos_pessoais/frontend_mentor/todo_app/script.js
@@ -1,6 +1,30 @@
 const task = document.querySelector('#task');
+const circleInput = document.querySelector('.circleInput');
+const mainTasks = document.querySelector('#mainTasks');
 const circle = [...document.querySelectorAll('.circle')];
+const cross = [...document.querySelectorAll('.cross')];
+const itemsQuantity = document.querySelector('#itemsQuantity');
+const clear = document.querySelector('#clear');
 
-circle.map((circle) => {
- 
+circle.forEach((circle) => {
+    circle.addEventListener("click", () => {
+        circle.classList.toggle('gradient');
+        circle.firstChild.nextSibling.classList.toggle('hide');
+        circle.nextSibling.nextSibling.classList.toggle('checked');
+    });
 });
+
+cross.forEach((cross) => {
+    cross.addEventListener("click", (e) => {
+        e.target.previousSibling.previousSibling.parentNode.remove();
+    });
+});
+
+clear.addEventListener("click", () => {
+    circle.map((el) => {
+        if (el.classList.contains('gradient')) {
+            el.parentNode.parentNode.remove();
+        };
+    });
+});
+
